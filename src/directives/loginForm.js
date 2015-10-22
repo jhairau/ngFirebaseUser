4d117ec8ea4da5407d037e873adfa99bfd21a833
Johnathan Hair <johnathan.hair.au@gmail.com>
2015-10-22 21:13:32 +1000

    Updated ionic form

--- src/directives/loginForm.js
+++ src/directives/loginForm.js
@@ -1,25 +1,32 @@
 angular.module('ngFirebaseUser')
-    .directive('ngFirebaseUserLoginForm', ['ngFirebaseUserConfig', 'ngFirebaseUserUser', function(ngFirebaseUserConfig, ngFirebaseUserUser) {
+    .directive('ngFirebaseUserLoginForm', ['ngFirebaseUserConfig', 'ngFirebaseUserUser', function (ngFirebaseUserConfig, ngFirebaseUserUser) {
         return {
             restrict: 'E',
             templateUrl: ngFirebaseUserConfig.get('templatePath') + '/login-form.html',
-            link: function(scope) {
+            link: function (scope) {
 
                 // Add to scope
                 angular.extend(scope, {
                     user: {
                         email: null,
                         password: null
-                    }
+                    },
+                    loginStatus: null
                 });
 
                 /**
                  * Attempt to login the user
                  * @return {[type]} [description]
                  */
-                scope.doLogin = function() {
+                scope.doLogin = function () {
                     ngFirebaseUserUser.loginEmail(scope.user.email, scope.user.password);
                 };
+
+
+                scope.$on('ngFirebaseUser:login_error', function (event, err) {
+                    console.log(event, err);
+                });
+
             }
         };
     }]);
